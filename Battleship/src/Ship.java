d26263fc874df49c9c337e5d273f946114dfa713
John Smith <nbwcoc@openmailbox.org>
2018-10-26 20:20:43 -0400

    ship

---
+++ Battleship/src/Ship.java
@@ -0,0 +1,20 @@
+public class Ship {
+	private int length;
+	private int hitCount = 0;
+	
+	public Ship(int length) {
+		this.length = length;
+	}
+	
+	public int getLength() {
+		return length;
+	}
+	
+	public void hit() {
+		hitCount++;
+	}
+	
+	public boolean isSunken() {
+		return hitCount == length;
+	}
+}
