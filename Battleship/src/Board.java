6b33181da640ac630ea01ee35c12f881e77b26be
Nick Bielinski <ndbielinski@gmail.com>
2018-11-18 19:37:44 -0500

    Placing ships on the board should now take into account the direction the ship is being placed in

--- Battleship/src/Board.java
+++ Battleship/src/Board.java
@@ -38,20 +38,20 @@ public class Board {
 		int shipCounter = 0;
 		// run our loop 5 times for the amount of ships we want placed
 		for (int x = 0; x < 5; x++) {
-			// create an x and y pairing for the initial placement of the ship
-			// direction will be used for ship placement. 0 = North, 1 = East, 2 = South, 3 = West
+			/* create an x and y pairing for the initial placement of the ship
+			direction will be used for ship placement. 0 = North, 1 = East, 2 = South, 3 = West */
 
 			int xCoord = rand.nextInt(10) + 1;
 			int yCoord = rand.nextInt(10) + 1;
 			int direction = rand.nextInt(4);
 			Integer[] xy = { xCoord, yCoord };
 			shipCounter = 0;
-			// cycle through a loop that is the size of the ship
-			// we will place the ship in its initial position
-			// then we will find what direction we are going with the ship, and increment or decrement the coordinates until the ship length has been reached
+			/*cycle through a loop that is the size of the ship
+			we will place the ship in its initial position
+			then we will find what direction we are going with the ship, and increment or decrement the coordinates until the ship length has been reached */
 			while(shipCounter < shipArray[x].getLength()) {
 				// if this specific coordinate on the board does not exist in the hashmap then we will place the ship
-				if (!shipLayer.containsKey(xy) && checkBounds(shipArray[x],xCoord,yCoord)) {
+				if (!shipLayer.containsKey(xy) && checkBounds(shipArray[x],xCoord,yCoord,direction)) {
 					shipLayer.put(xy, shipArray[x]);
 					shipCounter++;
 					if (direction == 0) {
@@ -79,11 +79,18 @@ public class Board {
 
 			}
 		}
+		// shipLayer.forEach((key, value) -> System.out.println(key.toString() + ":" + value.toString()));
 	}
 	
 	// this will make sure that our ship placement will not go out of the bounds of the board
-	private boolean checkBounds(Ship ship, int xCoord, int yCoord) {
-		if(ship.getLength() + xCoord <= 10 && ship.getLength() - xCoord >= 0 && ship.getLength() + yCoord <= 10 && ship.getLength() - yCoord >= 0)
+	private boolean checkBounds(Ship ship, int xCoord, int yCoord, int direction) {
+		if(ship.getLength() + yCoord <= 10 && direction == 0)
+			return true;
+		else if(ship.getLength() + xCoord <= 10 && direction == 1)
+			return true;
+		else if(ship.getLength() - yCoord >= 0 && direction == 2)
+			return true;
+		else if(ship.getLength() - xCoord >= 0 && direction == 3)
 			return true;
 		else
 			return false;
