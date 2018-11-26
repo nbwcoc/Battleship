57c67b8157186e224f8f10a27da51754d25195c4
John Smith <nbwcoc@openmailbox.org>
2018-11-25 22:47:26 -0500

    shoot ships

--- Battleship/src/HitsBoard.java
+++ Battleship/src/HitsBoard.java
@@ -6,9 +6,14 @@ public class HitsBoard {
 	public HitMarker tryHit(int x, int y) {
 		Integer[] xy = {x, y};
 		
+		System.out.printf("%d, %d %b", xy[0], xy[1], hits.containsKey(xy));
+		
 		if (hits.containsKey(xy))
 			return null;
 		
-		return hits.put(xy, new HitMarker(false));
+		var newHit = new HitMarker(false);
+		hits.put(xy,  newHit);
+		
+		return newHit;
 	}
 }
--- Battleship/src/Player.java
+++ Battleship/src/Player.java
@@ -9,6 +9,7 @@ public class Player {
 	private HitsBoard hitBoard = new HitsBoard();
 	private Board shipBoard = new Board();
 	private Ship[] ships = new Ship[5];
+	private boolean isHost = false;
 	
 	private boolean host(String x) {
 	    try {
@@ -18,6 +19,7 @@ public class Player {
             System.err.println("failed to start and connect");
             return false;
         }
+	    isHost = true;
         return true;
 	}
 	
@@ -57,10 +59,63 @@ public class Player {
 	    }
 	}
 	
+	private void shoot() {
+	    System.out.print("Enter coords (x y)");
+	    var s = new Scanner(System.in);
+        int coords[] = new int[2];
+        
+	    // I'm getting flashbacks to using GOTOs in BASIC
+	    while (true) {
+	        System.out.print("> ");
+	        var rawCoords = s.nextLine().split("[ ,]");
+	        
+	        if (rawCoords.length != 2) {
+	            System.out.println("Invalid length");
+	            continue;
+	        }
+
+	        boolean haveValidCoords = true;
+	        
+	        for (int i = 0; i < 2; i++) {
+	            try {
+	                coords[i] = Integer.parseInt(rawCoords[i]);
+	                assert coords[i] <= 10 && coords[i] > 0; 
+	            } catch (Exception e) {
+	                System.out.println("Invalid input");
+	                haveValidCoords = false;
+	                break;
+	            }
+	        }
+	        
+	        if (!haveValidCoords)
+	            continue;
+	        
+	        if (hitBoard.tryHit(coords[0], coords[1]) == null) {
+	            System.out.println("You already tried there");
+	            continue;
+	        }
+	        
+	        try {
+	            opponent.reportMove(new Move(coords[0], coords[1], Move.Type.SHOT));
+	        } catch (IOException e) {
+	            System.out.println("Something happened :(");
+	        }
+	        
+	        break;
+	    }
+	}
+	
+	private void mainLoop() {
+	    while (true) {
+	        shoot();
+	    }
+	}
+	
 	public static void main(String[] args) {
 	    Player player = new Player();
 	    
 	    player.start();
+	    player.mainLoop();
 	    
 	    System.out.println("\"That's all Folks!\"");
 	}
