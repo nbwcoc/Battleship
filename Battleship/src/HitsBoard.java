f1cc9e9035cffd13ad23afb3ce532efae69724a4
John Smith <nbwcoc@openmailbox.org>
2018-11-25 23:24:24 -0500

    rm debug output

--- Battleship/src/HitsBoard.java
+++ Battleship/src/HitsBoard.java
@@ -6,8 +6,6 @@ public class HitsBoard {
 	public HitMarker tryHit(int x, int y) {
 		var xy = new Coords(x, y);
 		
-		System.out.printf("%d, %d %b", xy[0], xy[1], hits.containsKey(xy));
-		
 		if (hits.containsKey(xy))
 			return null;
 		
